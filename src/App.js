e2b81ed13a271fda4a41a4276221068d62b77ce7
GuschinS <guschins@gmail.com>
2019-11-04 16:59:31 +0300

    add Loader

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import React from "react";
+import React, { useEffect } from "react";
 import TodoList from "./Todo/TodoList";
 import Context from "./context";
-import AddTodo from './Todo/AddTodo'
+import AddTodo from "./Todo/AddTodo";
+import Loader from './Loader'
 
 function App() {
   const [todos, setTodos] = React.useState([
-    { id: 1, completed: false, title: "Купить хлеб" },
-    { id: 2, completed: false, title: "Купить масло" },
-    { id: 3, completed: false, title: "Купить молоко" },
-    { id: 4, completed: false, title: "Купить лук" },
-    { id: 5, completed: false, title: "Купить помидор" },
-    { id: 6, completed: false, title: "Купить огурец" },
-    { id: 7, completed: false, title: "Купить яблоко" },
-    { id: 8, completed: false, title: "Купить пиццу" },
-    { id: 9, completed: false, title: "Купить шоколад" },
-    { id: 10, completed: false, title: "Купить сыр" },
-    { id: 11, completed: false, title: "Купить сосиски" },
-    { id: 12, completed: false, title: "Купить воду" }
-  ]);
+
+    // { id: 1, completed: false, title: "Купить хлеб" },
+    // { id: 2, completed: false, title: "Купить масло" },
+    // { id: 3, completed: false, title: "Купить молоко" },
+    // { id: 4, completed: false, title: "Купить лук" },
+    // { id: 5, completed: false, title: "Купить помидор" },
+    // { id: 6, completed: false, title: "Купить огурец" },
+    // { id: 7, completed: false, title: "Купить яблоко" },
+    // { id: 8, completed: false, title: "Купить пиццу" },
+    // { id: 9, completed: false, title: "Купить шоколад" },
+    // { id: 10, completed: false, title: "Купить сыр" },
+    // { id: 11, completed: false, title: "Купить сосиски" },
+    // { id: 12, completed: false, title: "Купить воду" }
+  ])
+  const [loading, setLoading] = React.useState(true)
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/todos?_limit=10")
+      .then(response => response.json())
+      .then(todos => {
+        setTimeout(() => {
+          setTodos(todos)
+          setLoading(false)
+        }, 500)
+        
+      });
+  }, []);
+
   function toggleTodo(id) {
     setTodos(
       todos.map(todo => {
@@ -34,11 +50,15 @@ function App() {
   }
 
   function addTodo(title) {
-    setTodos(todos.concat([{
-      title,
-      id: Date.now(),
-      completed: false
-    }]))
+    setTodos(
+      todos.concat([
+        {
+          title,
+          id: Date.now(),
+          completed: false
+        }
+      ])
+    );
   }
 
   return (
@@ -46,9 +66,10 @@ function App() {
       <div className="wrapper">
         <h1>React tutorial</h1>
         <AddTodo onCreate={addTodo} />
+        {loading && <Loader />}
         {todos.length ? (
           <TodoList todos={todos} onToggle={toggleTodo} />
-        ) : (
+        ) : loading ? null : (
           <p>No todos!</p>
         )}
       </div>
--- src/Todo/AddTodo.js
+++ src/Todo/AddTodo.js
@@ -1,20 +1,33 @@
 import React, {useState} from 'react'
 import PropTypes from 'prop-types'
 
-function AddTodo({onCreate}) {
-    const [value, setValue] = useState('')
+function useInputValue(defaultValue = '') {
+    const [value, setValue] = useState(defaultValue)
+    return {
+        bind: {
+            value,
+            onChange: event => setValue(event.target.value)
+        },
+        clear: () => setValue(''),
+        value: () => value
+    }
+        
+}
 
+function AddTodo({onCreate}) {
+    const input = useInputValue('')
     function submitHandler(event) {
         event.preventDefault()
-        if(value.trim()) {
-            onCreate(value)
-            setValue('') //clear input
+        if(input.value().trim()) {
+            onCreate(input.value())
+            // setValue('') //clear input
+            input.clear()
         }
     }
 
     return(
         <form style={{marginBottom: '.5rem'}} onSubmit = {submitHandler}>
-            <input type="text" value = {value} onChange={event => setValue(event.target.value)}/>
+            <input type="text" {...input.bind}/>
             <button type='submit'style={{marginLeft: '.5rem'}}>Add todo</button>
         </form>
     )
